eb94901c303521dc8285448b6e395b552b3e0f78
Jonathan <jonathan_guerreiro@hotmail.com>
2020-05-12 11:25:20 -0300

    Creación de Tablero, Region y Tipo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+
+function Casilla(props) {
+  const valor = props.value;
+  return (
+    <li>
+      <input id={valor} name={valor} type={props.type}/>
+      <label for={valor}>{valor}</label>
+    </li>
+  )
+}
+
+
+function Tipo(props) {
+  const tipos = props.tipos.map(tipo => 
+    <Casilla 
+      key={tipo} 
+      value={tipo} 
+      type="radio" 
+    />
+  )
+  return (
+      <ul>
+          <h2>Tipo</h2>
+          {tipos}
+      </ul>
+  )
+}
+
+
+function Region(props) {
+  const regiones = props.regiones.map(ele => 
+      <Casilla key={ele} value={ele} type="checkbox"/>
+    )
+  return (
+    <ul>
+      <h2>Region</h2>
+      {regiones}
+    </ul>
+  )
+}
+
+
+class Tablero extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        regiones: ["Demacia", "Freljord", "Jonia", "Noxus", "Piltover y Zaun", "Islas de la Sombra"],
+        tipos: ["Unidad", "Hechizo"],
+        rareza: ["Común", "Rara", "Épica", "Campeón"],
+        coste: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }
+  }
+
+  render() {
+      return (
+          <div>
+            <Region regiones={this.state.regiones} />
+            <Tipo tipos={this.state.tipos} />
+          </div>
+      )
+  }
+}   
+
+
+// ==================================
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Tablero />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
